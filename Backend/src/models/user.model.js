69d30b4e006baef9288b4226ec09eb17613b60da
Pranav chandan <challapranavchandan@gmail.com>
2026-05-25 09:54:59 +0530

    feat: initialize frontend boilerplate and define user mongoose schema with authentication methods

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -17,24 +17,63 @@ const userSchema = new mongoose.Schema({
         unique: true,
         trim: true,
         lowercase: true,
-        
+
     },
-     firstname: {
+    firstname: {
         type: String,
         required: true,
         trim: true,
         index: true,
-     },
-      lastname: {
+    },
+    lastname: {
         type: String,
         required: true,
         trim: true,
         index: true,
-     },
-     
+    },
+
 })
 
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next;
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next;
+});
 
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            id: this._id,
+            username: this.username,
+            email: this.email,
+            fullname: this.fullname,
+
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN
+        }
+    );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN
+        }
+    );
+};
 
 
 
